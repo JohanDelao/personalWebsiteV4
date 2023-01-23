a3d55b46eb27ddfe839b55ce0cb466f4ef3bb866
Johan Delao <johandelao10@gmail.com>
2023-01-22 22:51:02 -0500

    utilized useState more to decrease code and make it cleaner

--- src/components/experience.js
+++ src/components/experience.js
@@ -1,78 +1,21 @@
 import { useState } from "react";
 
 function Experience() {
-    const [experience, setExperience] = useState({title: "Website Developer", company: " @ Research Foundation CUNY", date: "June 2022 - Present", bulletOne: "Update and maintain the CEESP program website using WordPress, CSS, and HTML.", bulletTwo: "Troubleshoot bugs from the existing website to decrease load times by 10%.", bulletThree: "Improved user experience and increased applications by 16% by experimenting with design changes."});
+    const [experience, setExperience] = useState({title: "Website Developer", company: " @ Research Foundation CUNY", date: "June 2022 - Present", bulletOne: "Update and maintain the CEESP program website using WordPress, CSS, and HTML.", bulletTwo: "Troubleshoot bugs from the existing website to decrease load times by 10%.", bulletThree: "Improved user experience and increased applications by 16% by experimenting with design changes.", codePathSelect: false, cunySelect: true, metaSelect: false});
 
     function DisplayCodePath(){
-        const experienceOne = document.getElementById("experienceOne");
-        const experienceTwo = document.getElementById("experienceTwo");
-        const experienceThree = document.getElementById("experienceThree");
-        const barOne = document.getElementById("barOne");
-        const barTwo = document.getElementById("barTwo");
-        const barThree = document.getElementById("barThree");
-
-        if(experienceOne.classList.contains("selected")){
-            experienceOne.classList.remove("selected")
-            barOne.classList.remove("selected");
-        }
-        if(experienceTwo.classList.contains("selected")){
-            experienceTwo.classList.remove("selected");
-            barTwo.classList.remove("selected")
-        }
-
-        experienceThree.classList.add("selected");
-        barThree.classList.add("selected")
-
-        setExperience(prevExperience => {
-            return {title: "Android Development Fellow", company: " @ CodePath", date: "Jan. 2021 - May 2021", bulletOne: "Worked in a team of four to develop an Android application using Agile practices.", bulletTwo: "Developed 5+ Android applications using Android Studio, Java, Github and other in-demand technologies.", bulletThree: "Worked on around 3 features in a fast-paced setting in biweekly sprints."};
+        setExperience( () => {
+            return {title: "Android Development Fellow", company: " @ CodePath", date: "Jan. 2021 - May 2021", bulletOne: "Worked in a team of four to develop an Android application using Agile practices.", bulletTwo: "Developed 5+ Android applications using Android Studio, Java, Github and other in-demand technologies.", bulletThree: "Worked on around 3 features in a fast-paced setting in biweekly sprints.",codePathSelect: true, cunySelect: false, metaSelect: false};
         });
     }
     function DisplayCUNY(){
-        const experienceOne = document.getElementById("experienceOne");
-        const experienceTwo = document.getElementById("experienceTwo");
-        const experienceThree = document.getElementById("experienceThree");
-        const barOne = document.getElementById("barOne");
-        const barTwo = document.getElementById("barTwo");
-        const barThree = document.getElementById("barThree");
-
-        if(experienceTwo.classList.contains("selected")){
-            experienceTwo.classList.remove("selected")
-            barTwo.classList.remove("selected");
-        }
-        if(experienceThree.classList.contains("selected")){
-            experienceThree.classList.remove("selected");
-            barThree.classList.remove("selected")
-        }
-
-        experienceOne.classList.add("selected");
-        barOne.classList.add("selected")
-
-        setExperience(prevExperience => {
-            return {title: "Website Developer", company: " @ Research Foundation CUNY", date: "June 2022 - Present", bulletOne: "Update and maintain the CEESP program website using WordPress, CSS, and HTML.", bulletTwo: "Troubleshoot bugs from the existing website to decrease load times by 10%.", bulletThree: "Improved user experience and increased applications by 16% by experimenting with design changes."};
+        setExperience( () => {
+            return {title: "Website Developer", company: " @ Research Foundation CUNY", date: "June 2022 - Present", bulletOne: "Update and maintain the CEESP program website using WordPress, CSS, and HTML.", bulletTwo: "Troubleshoot bugs from the existing website to decrease load times by 10%.", bulletThree: "Improved user experience and increased applications by 16% by experimenting with design changes.", codePathSelect: false, cunySelect: true, metaSelect: false};
         });
     }
     function DisplayMeta(){
-        const experienceOne = document.getElementById("experienceOne");
-        const experienceTwo = document.getElementById("experienceTwo");
-        const experienceThree = document.getElementById("experienceThree");
-        const barOne = document.getElementById("barOne");
-        const barTwo = document.getElementById("barTwo");
-        const barThree = document.getElementById("barThree");
-
-        if(experienceOne.classList.contains("selected")){
-            experienceOne.classList.remove("selected")
-            barOne.classList.remove("selected");
-        }
-        if(experienceThree.classList.contains("selected")){
-            experienceThree.classList.remove("selected");
-            barThree.classList.remove("selected")
-        }
-
-        experienceTwo.classList.add("selected");
-        barTwo.classList.add("selected")
-
-        setExperience(prevExperience => {
-            return {title: "ABCS Program", company: "@ Meta", date: "Aug 2022 - Nov 2022", bulletOne: "Participated in a 9-week program focused on mastering critical data structures and algorithms.", bulletTwo: "Engaged by answering interview-type questions with peers and engineers.", bulletThree: "Networked with Meta engineers to learn best practices in software engineering."};
+        setExperience( () => {
+            return {title: "ABCS Program", company: "@ Meta", date: "Aug 2022 - Nov 2022", bulletOne: "Participated in a 9-week program focused on mastering critical data structures and algorithms.", bulletTwo: "Engaged by answering interview-type questions with peers and engineers.", bulletThree: "Networked with Meta engineers to learn best practices in software engineering.", codePathSelect: false, cunySelect: false, metaSelect: true};
         });
 
     }
@@ -83,16 +26,16 @@ function Experience() {
                 <div className="ExperienceExperiment">
                     <div className="leftExperience">
                         <div className="leftBarExperience">
-                            <div className="bar selected" id="barOne"></div>
-                            <p className="ExperienceName selected" id="experienceOne" onClick={DisplayCUNY}>Research Foundation CUNY</p>
+                            <div className={experience.cunySelect ? "bar selected":"bar"} id="barOne"></div>
+                            <p className={experience.cunySelect ? "ExperienceName selected":"ExperienceName"} id="experienceOne" onClick={DisplayCUNY}>Research Foundation CUNY</p>
                         </div>
                         <div className="leftBarExperience">
-                            <div className="bar" id="barTwo"></div>
-                            <p className="ExperienceName" id="experienceTwo" onClick={DisplayMeta}>Meta</p>
+                            <div className={experience.metaSelect ? "bar selected":"bar"} id="barTwo"></div>
+                            <p className={experience.metaSelect ? "ExperienceName selected":"ExperienceName"} id="experienceTwo" onClick={DisplayMeta}>Meta</p>
                         </div>
                         <div className="leftBarExperience">
-                            <div className="bar" id="barThree"></div>
-                            <p className="ExperienceName" onClick={DisplayCodePath} id="experienceThree">CodePath </p>
+                            <div className={experience.codePathSelect ? "bar selected":"bar"} id="barThree"></div>
+                            <p className={experience.codePathSelect ? "ExperienceName selected":"ExperienceName"} onClick={DisplayCodePath} id="experienceThree">CodePath </p>
                         </div>
                     </div>
                     <div className="rightExperience">
