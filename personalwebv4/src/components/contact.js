f25c25bc6dbad657ab1f659967c8d180441fb16c
Johan Delao <johandelao10@gmail.com>
2023-02-01 19:50:25 -0500

    fixed some responsive issues

--- personalwebv4/src/components/contact.js
+++ personalwebv4/src/components/contact.js
@@ -4,7 +4,7 @@ function Contact() {
             <div className="inner">
                 <p className="mediumHeader">Want to chat?</p>
                 <p className="smallP contactMessage">Have a question? Want to give some feedback? Want to say hi? Feel free to send me a message or contact me on LinkedIn!</p>
-                <button id="sendMessage">SEND MESSAGE</button>
+                <button id="sendMessage"><a href="mailto:johandelao10@gmail.com">SEND MESSAGE</a></button>
             </div>
         </div>
     )
